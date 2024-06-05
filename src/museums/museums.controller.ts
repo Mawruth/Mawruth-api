99b537bbbeeb4aaa59ca9183ae413a06d62584fa
PandaX185 <mouss3abbb@gmail.com>
2024-06-05 11:17:00 +0300

    add museum crud operations

--- src/museums/museums.controller.ts
+++ src/museums/museums.controller.ts
@@ -1,12 +1,124 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Delete, UseGuards, Query, Param, Put } from '@nestjs/common';
 import { CreateMuseumDto } from './dto/create-museum.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { MuseumsService } from './museums.service';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { EditMuseumDto } from './dto/edit-museum.dto';
+import { UserTypeGuard } from 'src/guards/user-type.guard';
+import { UserTypes } from 'src/decorators/userTypes.decorato';
 
 @ApiTags('museums')
 @Controller('museums')
 export class MuseumsController {
+  constructor(private readonly service: MuseumsService) { }
+
+  @UseGuards(AuthGuard, UserTypeGuard)
+  @UserTypes("SUPPER_ADMIN")
   @Post()
-  create(@Body() createMuseumDto: CreateMuseumDto) {
-    return createMuseumDto;
+  @ApiOperation({
+    summary: 'Create new museum',
+  })
+  @ApiBearerAuth()
+  async create(@Body() createMuseumDto: CreateMuseumDto) {
+    return this.service.createMuseum(createMuseumDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get()
+  @ApiOperation({
+    summary: 'Get all museums',
+    parameters: [
+      {
+        name: 'page',
+        in: 'query',
+        description: 'Page number',
+        required: false,
+        schema: { type: 'number' }
+      },
+      {
+        name: 'limit',
+        in: 'query',
+        description: 'Limit number',
+        required: false,
+        schema: { type: 'number' }
+      },
+      {
+        name: 'search',
+        in: 'query',
+        description: 'Search by name',
+        required: false,
+        schema: { type: 'string' }
+      },
+    ]
+  })
+  @ApiBearerAuth()
+  async getAll(
+    @Query() page: number,
+    @Query() limit: number,
+    @Query() search: string,
+  ) {
+    return this.service.getAllMuseums(page, limit, search);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Get museum by id',
+    parameters: [
+      {
+        name: 'id',
+        in: 'path',
+        description: 'Museum id',
+        required: true,
+        schema: { type: 'number' }
+      },
+    ]
+  })
+  @ApiBearerAuth()
+  async getById(@Param('id') id: number) {
+    return this.service.getMuseumById(id);
+  }
+
+  @UseGuards(AuthGuard, UserTypeGuard)
+  @UserTypes("SUPPER_ADMIN")
+  @Put(':id')
+  @ApiOperation({
+    summary: 'Edit museum by id',
+    parameters: [
+      {
+        name: 'id',
+        in: 'path',
+        description: 'Museum id',
+        required: true,
+        schema: { type: 'number' }
+      },
+    ]
+  })
+  @ApiBearerAuth()
+  async editMuseum(
+    @Param('id') id: number,
+    @Body() editMuseumDto: EditMuseumDto,
+  ) {
+    return this.service.editMuseum(id, editMuseumDto);
+  }
+
+  @UseGuards(AuthGuard, UserTypeGuard)
+  @UserTypes("SUPPER_ADMIN")
+  @Delete(':id')
+  @ApiOperation({
+    summary: 'Delete museum by id',
+    parameters: [
+      {
+        name: 'id',
+        in: 'path',
+        description: 'Museum id',
+        required: true,
+        schema: { type: 'number' }
+      },
+    ]
+  })
+  @ApiBearerAuth()
+  async deleteMuseum(@Param('id') id: number) {
+    return this.service.deleteMuseum(id);
   }
 }
--- src/museums/museums.service.ts
+++ src/museums/museums.service.ts
@@ -1,4 +1,100 @@
 import { Injectable } from '@nestjs/common';
+import { Museums } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { EditMuseumDto } from './dto/edit-museum.dto';
+import { handlePrismaError } from 'src/utils/prisma-error.util';
+import { CreateMuseumDto } from './dto/create-museum.dto';
 
 @Injectable()
-export class MuseumsService {}
+export class MuseumsService {
+	constructor(private readonly prismaService: PrismaService) { }
+
+	async getAllMuseums(
+		page: number,
+		limit: number,
+		search: string,
+	): Promise<Museums[]> {
+		let res: Promise<Museums[]>
+		try {
+			res = this.prismaService.museums.findMany(
+				{
+					skip: (page - 1) * limit,
+					take: limit,
+					where: {
+						name: {
+							contains: search,
+							mode: 'insensitive',
+						},
+					},
+					orderBy: {
+						id: 'desc',
+					},
+				}
+			);
+		} catch (error) {
+			handlePrismaError(error);
+		}
+		return res;
+	}
+
+	async getMuseumById(id: number): Promise<Museums> {
+		let res: Promise<Museums>
+		try {
+			res = this.prismaService.museums.findUnique({
+				where: {
+					id: id,
+				},
+			});
+		} catch (error) {
+			handlePrismaError(error);
+		}
+		return res;
+	}
+
+	async createMuseum(museum: CreateMuseumDto): Promise<Museums> {
+		let res: Promise<Museums>
+		try {
+			res = this.prismaService.museums.create({
+				data: museum,
+			});
+		} catch (error) {
+			handlePrismaError(error);
+		}
+		return res;
+	}
+
+	async editMuseum(id: number, data: EditMuseumDto): Promise<Museums> {
+		let res: Promise<Museums>
+		try {
+			res = this.prismaService.museums.update({
+				where: {
+					id: id,
+				},
+				data: {
+					name: data.name,
+					description: data.description,
+					city: data.city,
+					street: data.street,
+				},
+			});
+		} catch (error) {
+			handlePrismaError(error);
+		}
+		return res;
+	}
+
+	async deleteMuseum(id: number): Promise<Museums> {
+		let res: Promise<Museums>
+		try {
+			res = this.prismaService.museums.delete({
+				where: {
+					id: id,
+				},
+			});
+		} catch (error) {
+			handlePrismaError(error);
+		}
+		return res;
+	}
+
+}
