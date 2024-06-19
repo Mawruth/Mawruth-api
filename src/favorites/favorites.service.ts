dd29551d82697998aa94e3756a614021ce17cace
Ahmed Mabrouk <said66913@gmail.com>
2024-06-19 14:53:54 +0300

    add favorites functions

---
+++ src/favorites/favorites.service.ts
@@ -0,0 +1,158 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { handlePrismaError } from 'src/utils/prisma-error.util';
+
+@Injectable()
+export class FavoritesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getMuseum(userId: number) {
+    try {
+      return await this.prisma.museumsFavorites.findMany({
+        where: {
+          userId,
+        },
+        include: {
+          museum: {
+            select: {
+              id: true,
+              name: true,
+              images: {
+                select: {
+                  image_path: true,
+                },
+              },
+            },
+          },
+        },
+      });
+    } catch (error) {
+      handlePrismaError(error);
+    }
+  }
+
+  async addMuseum(userId: number, museumId: number) {
+    try {
+      const isFav = await this.prisma.museumsFavorites.findFirst({
+        where: {
+          userId,
+          museumId,
+        },
+      });
+
+      if (isFav) {
+        throw new BadRequestException(
+          'user already added this museum to favorites list',
+        );
+      }
+
+      await this.prisma.museumsFavorites.create({
+        data: {
+          userId,
+          museumId,
+        },
+      });
+    } catch (error) {
+      handlePrismaError(error);
+    }
+  }
+
+  async deleteMuseum(userId: number, museumId: number) {
+    try {
+      const isFav = await this.prisma.museumsFavorites.findMany({
+        where: {
+          userId,
+          museumId,
+        },
+      });
+
+      if (!isFav.length) {
+        throw new NotFoundException('This museum not added in favorites list');
+      }
+
+      await this.prisma.museumsFavorites.deleteMany({
+        where: {
+          userId,
+          museumId,
+        },
+      });
+    } catch (error) {
+      handlePrismaError(error);
+    }
+  }
+
+  async getPiece(userId: number) {
+    try {
+      return await this.prisma.piecesFavorites.findMany({
+        where: {
+          userId,
+        },
+        include: {
+          piece: {
+            select: {
+              id: true,
+              name: true,
+              image: true,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      handlePrismaError(error);
+    }
+  }
+
+  async addPiece(userId: number, pieceId: number) {
+    try {
+      const isFav = await this.prisma.piecesFavorites.findFirst({
+        where: {
+          userId,
+          pieceId,
+        },
+      });
+
+      if (isFav) {
+        throw new BadRequestException(
+          'user already added this piece to favorites list',
+        );
+      }
+
+      await this.prisma.piecesFavorites.create({
+        data: {
+          userId,
+          pieceId,
+        },
+      });
+    } catch (error) {
+      handlePrismaError(error);
+    }
+  }
+
+  async deletePiece(userId: number, pieceId: number) {
+    try {
+      const isFav = await this.prisma.piecesFavorites.findMany({
+        where: {
+          userId,
+          pieceId,
+        },
+      });
+
+      if (!isFav.length) {
+        throw new NotFoundException('This piece not added in favorites list');
+      }
+
+      await this.prisma.piecesFavorites.deleteMany({
+        where: {
+          userId,
+          pieceId,
+        },
+      });
+    } catch (error) {
+      handlePrismaError(error);
+    }
+  }
+}
